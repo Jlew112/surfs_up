f20c0d3542e3a1b0c56b0ea7581f7bf1f6adeca7
Jlew112 <87906387+Jlew112@users.noreply.github.com>
2021-10-06 21:23:28 -0600

    Add files via upload

--- surfs_up/app.py
+++ surfs_up/app.py
@@ -87,6 +87,6 @@ def stats(start=None, end=None):
 #def hello_world():
 #    return 'Hello World'
 
-#if __name__ == "__main__":
-#    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
